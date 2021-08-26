f0c75fd4e6726ae0a0d2cfb7aa1d063ceea1a43b
Changseo Jang <changseo.jang@riiid.co>
2021-08-26 18:48:00 +0900

    improve cost

--- convert-planner/src/index.ts
+++ convert-planner/src/index.ts
@@ -1,19 +1,44 @@
 import { Type as Schema } from "@riiid/cabriolet-proto/lib/messages/riiid/kvf/Schema";
 import { Type as ConvertPlan } from "@riiid/cabriolet-proto/lib/messages/riiid/kvf/ConvertPlan";
+import { Ord, Ordering } from "./util/Ord";
 
-type converterCost = number
-type upcastCost = number
+class Cost extends Ord {
+  converterCost: number;
+  upcastCost: number;
+
+  constructor(converterCost: number, upcastCost: number) {
+    super()
+    this.converterCost = converterCost;
+    this.upcastCost = upcastCost;
+  }
+
+  override compare(b: Cost): Ordering {
+    if (this.converterCost < b.converterCost) {
+      return Ordering.LT;
+    } else if (this.converterCost > b.converterCost) {
+      return Ordering.GT;
+    } else {
+      if (this.upcastCost < b.upcastCost) {
+        return Ordering.LT;
+      } else if (this.upcastCost > b.upcastCost) {
+        return Ordering.GT;
+      } else {
+        return Ordering.EQ;
+      }
+    }
+  }
+}
 
 interface Edge {
   from: string,
   to: string,
-  cost: [converterCost, upcastCost],
+  cost: Cost,
 }
 
 export class ConvertPlannerError extends Error {
   constructor(msg: string, from: string, to: string) {
-    super(msg)
-    this.name = `failed to find path "${from}" to "${to}"`
+    super(msg);
+    this.name = `failed to find path "${from}" to "${to}"`;
   }
 }
 
@@ -26,27 +51,27 @@ export default function plan(
     return {
       from: it.fromFormatId,
       to: it.toFormatId,
-      cost: [1, 0],
+      cost: new Cost(1, 0),
     }
-  })
+  });
   const upcasts: Edge[] = schema.formats.flatMap(it => {
     return it.parentFormatIds.map(parentFormatId => {
       return {
         from: it.id,
         to: parentFormatId,
-        cost: [0, 1],
+        cost: new Cost(0, 1),
       }
     })
-  })
+  });
 
-  const converterGraphEdges: Edge[] = [...upcasts, ...edges]
+  const converterGraphEdges: Edge[] = [...upcasts, ...edges];
+
+  const fromExists: boolean = converterGraphEdges.reduce((prev, curr) => prev || (curr.from === fromFormatId), false);
+  const toExists: boolean = converterGraphEdges.reduce((prev, curr) => prev || (curr.to === toFormatId), false);
+
+  if (!(fromExists && toExists)) throw new ConvertPlannerError("The input does not exists in the convert graph", fromFormatId, toFormatId);
 
   console.log(converterGraphEdges)
-
-  const fromExists: boolean = converterGraphEdges.reduce((prev, curr) => prev || (curr.from === fromFormatId), false)
-  const toExists: boolean = converterGraphEdges.reduce((prev, curr) => prev || (curr.to === toFormatId), false)
-
-  if (!(fromExists && toExists)) throw new ConvertPlannerError("The input does not exists in the convert graph", fromFormatId, toFormatId)
   
   return {
     fromFormatId,
---
+++ convert-planner/src/util/Ord.ts
@@ -0,0 +1,43 @@
+export enum Ordering {
+  LT,
+  EQ,
+  GT,
+}
+
+export abstract class Ord {
+  abstract compare(b: Ord): Ordering;
+
+  isBigger(b: Ord): boolean {
+    return Ordering.GT === this.compare(b)
+  }
+
+  isSmaller(b: Ord): boolean {
+    return Ordering.LT === this.compare(b)
+  }
+}
+
+export function compare<T extends Ord>(a: T, b: T): Ordering {
+  return a.compare(b)
+}
+
+export function min<T extends Ord>(a: T, b: T): T {
+    switch (a.compare(b)) {
+      case Ordering.LT:
+        return a
+      case Ordering.GT:
+        return b
+      case Ordering.EQ:
+        return a
+    }
+}
+
+export function max<T extends Ord>(a: T, b: T): T {
+    switch (a.compare(b)) {
+      case Ordering.GT:
+        return a
+      case Ordering.LT:
+        return b
+      case Ordering.EQ:
+        return a
+    }
+  }
