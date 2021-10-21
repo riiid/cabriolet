744fac18da584f5dddc4c3876f7481a6870ec109
jay <jaykang1992@naver.com>
2021-10-21 15:53:43 -0700

    Finish Postgres

---
+++ service-impl/azure/src/registry.test.ts
@@ -0,0 +1,329 @@
+import {Pool, PoolClient} from "pg";
+import createAzureRegistry from "./registry";
+import {PostgresDatabaseConfig} from "./postgresDatabaseConfig";
+import {Registry} from "../../../service";
+import {migrate} from "postgres-migrations";
+import * as path from "path"
+import {Schema} from "../../../proto/lib/messages/riiid/kvf";
+import * as crypto from "crypto";
+
+
+let pool: Pool
+let registry: Registry
+const dbName = "postgres"
+const user = "postgres"
+const pwd = "yourpassword"
+const host = "localhost"
+const port = "5432"
+const migrationsDirectory = path.resolve("./src/migrations")
+const connectionSting = `postgres://${user}:${pwd}@${host}:${port}/${dbName}`
+
+beforeAll(async () => {
+  pool = await new Pool({
+    connectionString: connectionSting
+  })
+  const config: PostgresDatabaseConfig = {
+    pool: pool
+  }
+
+  const client = await config.pool.connect()
+  try {
+    await migrate({client}, migrationsDirectory)
+  } finally {
+    await client.release()
+  }
+
+  registry = createAzureRegistry(config)
+})
+
+afterEach(async () => {
+  const client = await pool.connect()
+  const sql = `DELETE
+               FROM formats;
+  DELETE
+  FROM edges;
+  DELETE
+  FROM converters;
+  DELETE
+  FROM validators`
+  await client.query(sql)
+  await client.release()
+})
+
+afterAll( () => {})
+
+describe("CreateRegistryTest", () => {
+  describe("getSchema", () => {
+    it("when get schema then gets successfully", async () => {
+      // when
+      const res = await registry.getSchema({})
+
+      // then
+      expect(res.schema!.formats).toEqual([])
+      expect(res.schema!.edges).toEqual([])
+      expect(res.schema!.converters).toEqual([])
+      expect(res.schema!.validators).toEqual([])
+    })
+  })
+
+  describe("createFormat", () => {
+    it("when create format then format is added to schema successfully", async () => {
+      // given
+      const formatName = crypto.randomUUID() + "_formatName"
+      const formatDescription = crypto.randomUUID() + "_formatDescription"
+
+      // when
+      await registry.createFormat({
+        formatName: formatName,
+        formatDescription: formatDescription
+      })
+
+      const res = await registry.getSchema({})
+
+      // then
+      expect(res.schema!!.formats[0].name).toEqual(formatName)
+      expect(res.schema!!.formats[0].description).toEqual(formatDescription)
+    })
+  })
+
+  describe("deleteFormat", () => {
+    it("when delete format then format does not exist in formats", async () => {
+      // given
+      const formatName = crypto.randomUUID() + "_formatName"
+      const formatDescription = crypto.randomUUID() + "_formatDescription"
+      const {formatId} = await registry.createFormat({
+        formatName: formatName,
+        formatDescription: formatDescription
+      })
+
+      // when
+      const beforeSchema = await registry.getSchema({})
+      await registry.deleteFormat({formatId})
+      const afterSchema = await registry.getSchema({})
+
+      // then
+      expect(beforeSchema.schema!!.formats[0].id).toEqual(formatId)
+      expect(beforeSchema.schema!!.formats[0].name).toEqual(formatName)
+      expect(beforeSchema.schema!!.formats[0].description).toEqual(formatDescription)
+      expect(afterSchema.schema!!.formats.length).toEqual(0)
+    })
+  })
+
+  describe("setParent", () => {
+    it("when set parent then parent format id is added successfully in the format", async () => {
+      // given
+      const formatName = crypto.randomUUID() + "_formatName"
+      const formatDescription = crypto.randomUUID() + "_formatDescription"
+      const parentFormatId = crypto.randomUUID() + "_parentFormatId"
+      const {formatId} = await registry.createFormat({
+        formatName: formatName,
+        formatDescription: formatDescription
+      })
+
+      // when
+      registry.setParent({
+        formatId: formatId,
+        parentFormatId: parentFormatId
+      })
+      const res = await registry.getSchema({})
+
+      // then
+      console.log(res.schema!.formats)
+      expect(res.schema!!.formats[0].parentFormatId).toEqual(parentFormatId)
+      // res.then(schema => {
+      //     expect(schema.schema!!.formats[0].parentFormatId).toEqual(parentFormatId)
+      //
+      // }
+      // )
+      expect(res.schema!!.formats[0].id).toEqual(formatId)
+      // res.then(schema => expect(schema.schema!!.formats[0].id).toEqual(formatId))
+    })
+  })
+
+  describe("deleteParent", () => {
+    it("when delete parent then parent format id does not exist in the format", () => {
+      // given
+      const formatName = crypto.randomUUID() + "_formatName"
+      const formatDescription = crypto.randomUUID() + "_formatDescription"
+      const parentFormatId = crypto.randomUUID() + "_parentFormatId"
+      registry.createFormat({
+        formatName: formatName,
+        formatDescription: formatDescription
+      })
+        .then(res => {
+          const formatId = res.formatId
+          registry.setParent({
+            formatId: formatId,
+            parentFormatId: parentFormatId
+          })
+
+          // when
+          const beforeSchema = registry.getSchema({})
+          registry.deleteParent({
+            formatId: formatId
+          })
+          const afterSchema = registry.getSchema({})
+
+          // then
+          beforeSchema.then(schema => expect(schema.schema!!.formats[0].parentFormatId).toEqual(parentFormatId))
+          beforeSchema.then(schema => expect(schema.schema!!.formats[0].id).toEqual(formatId))
+          afterSchema.then(schema => expect(schema.schema!!.formats[0].parentFormatId).toEqual("undefined"))
+        })
+    })
+  })
+
+  describe("appendValidator", () => {
+    it("when create validator then validatorId is added in format successfully and validator is added successfully", async () => {
+      // given
+      const formatName = crypto.randomUUID() + "_formatName"
+      const formatDescription = crypto.randomUUID() + "_formatDescription"
+      const validatorName = crypto.randomUUID() + "_validatorDescription"
+      const validatorDescription = crypto.randomUUID() + "_validatorDescription"
+      const validationSrc = crypto.randomUUID() + "_validatorSrc"
+      const validationIntegrity = crypto.randomUUID() + "_validatorIntegrity"
+      const {formatId} = await registry.createFormat({
+        formatName: formatName,
+        formatDescription: formatDescription
+      })
+
+      // when
+      registry.appendValidator({
+        formatId: formatId,
+        validatorName: validatorName,
+        validatorDescription: validatorDescription,
+        validatorSrc: validationSrc,
+        validatorIntegrity: validationIntegrity
+      })
+        .then(res => {
+          const validatorId = res.validatorId
+          registry.getSchema({})
+            .then(res => {
+              const schema = res.schema
+
+              // then
+              const validator = schema!.validators[0]
+              expect(schema!!.formats[0].validatorIds[0]).toBe(validatorId.toString())
+              expect(validator.id).toEqual(validatorId)
+              expect(validator.name).toEqual(validatorName)
+              expect(validator.description).toEqual(validatorDescription)
+              expect(validator.src).toEqual(validationSrc)
+              expect(validator.integrity).toEqual(validationIntegrity)
+            })
+        })
+    })
+  })
+
+  describe("removeValidator", () => {
+    it("when delete validator then validatorId is deleted from format and validator is deleted successfully", async () => {
+      // given
+      const formatName = crypto.randomUUID() + "_formatName"
+      const formatDescription = crypto.randomUUID() + "_formatDescription"
+      const validatorName = crypto.randomUUID() + "_validatorDescription"
+      const validatorDescription = crypto.randomUUID() + "_validatorDescription"
+      const validationSrc = crypto.randomUUID() + "_validatorSrc"
+      const validationIntegrity = crypto.randomUUID() + "_validatorIntegrity"
+      const {formatId} = await registry.createFormat({
+        formatName: formatName,
+        formatDescription: formatDescription
+      })
+      const {validatorId} = await registry.appendValidator({
+        formatId: formatId,
+        validatorName: validatorName,
+        validatorDescription: validatorDescription,
+        validatorSrc: validationSrc,
+        validatorIntegrity: validationIntegrity
+      })
+
+      // when
+      const beforeSchema = await registry.getSchema({})
+      await registry.removeValidator({
+        formatId: formatId,
+        validatorId: validatorId
+      })
+      const afterSchema = await registry.getSchema({})
+
+      // then
+      const validator = beforeSchema.schema!!.validators[0]
+      expect(validator.id).toEqual(validatorId)
+      expect(afterSchema.schema!!.validators.length).toEqual(0)
+      expect(afterSchema.schema!!.formats[0].validatorIds.length).toEqual(0)
+    })
+  })
+
+  describe("createConverter", () => {
+    it("when create converter then edge and converter are added", async () => {
+      // given
+      const formatName = crypto.randomUUID() + "_formatName"
+      const formatDescription = crypto.randomUUID() + "_formatDescription"
+      const fromFormatId = (await registry.createFormat({
+        formatName: formatName,
+        formatDescription: formatDescription
+      })).formatId
+      const toFormatId = (await registry.createFormat({
+        formatName: formatName,
+        formatDescription: formatDescription
+      })).formatId
+      const converterName = crypto.randomUUID() + "_converterName"
+      const converterDescription = crypto.randomUUID() + "_converterDescription"
+      const converterSrc = crypto.randomUUID() + "_converterSrc"
+      const converterIntegrity = crypto.randomUUID() + "_converterIntegrity"
+
+      // when
+      const converterId = await registry.createConverter({
+        fromFormatId: fromFormatId,
+        toFormatId: toFormatId,
+        converterName: converterName,
+        converterDescription: converterDescription,
+        converterSrc: converterSrc,
+        converterIntegrity: converterIntegrity
+      })
+      const schema = await registry.getSchema({})
+
+      // then
+      expect(schema.schema!!.edges[0].toFormatId).toEqual(toFormatId.toString())
+      expect(schema.schema!!.edges[0].fromFormatId).toEqual(fromFormatId.toString())
+      expect(schema.schema!!.edges[0].converterId).toEqual(converterId.converterId.toString())
+      expect(schema.schema!!.converters[0].id).toEqual(converterId.converterId)
+      expect(schema.schema!!.converters[0].name).toEqual(converterName)
+      expect(schema.schema!!.converters[0].src).toEqual(converterSrc)
+      expect(schema.schema!!.converters[0].integrity).toEqual(converterIntegrity)
+    })
+  })
+
+  describe("deleteConverter", () => {
+    it("when delete converter then edge and converter are deleted", async () => {
+      // given
+      const formatName = crypto.randomUUID() + "_formatName"
+      const formatDescription = crypto.randomUUID() + "_formatDescription"
+      const fromFormatId = (await registry.createFormat({
+        formatName: formatName,
+        formatDescription: formatDescription
+      })).formatId
+      const toFormatId = (await registry.createFormat({
+        formatName: formatName,
+        formatDescription: formatDescription
+      })).formatId
+      const converterName = crypto.randomUUID() + "_converterName"
+      const converterDescription = crypto.randomUUID() + "_converterDescription"
+      const converterSrc = crypto.randomUUID() + "_converterSrc"
+      const converterIntegrity = crypto.randomUUID() + "_converterIntegrity"
+      const {converterId} = await registry.createConverter({
+        fromFormatId: fromFormatId,
+        toFormatId: toFormatId,
+        converterName: converterName,
+        converterDescription: converterDescription,
+        converterSrc: converterSrc,
+        converterIntegrity: converterIntegrity
+      })
+
+      // when
+      await registry.deleteConverter({converterId})
+      const schema = await registry.getSchema({})
+      
+      // then
+      expect(schema.schema!!.edges.length).toEqual(0)
+      expect(schema.schema!!.converters.length).toEqual(0)
+    })
+  })
+})
+
---
+++ service-impl/azure/src/registryRepository.ts
@@ -0,0 +1,186 @@
+import {Pool} from "pg";
+import {
+  AppendValidatorRequest, Converter, CreateConverterRequest,
+  CreateFormatRequest, DeleteConverterRequest,
+  DeleteFormatRequest,
+  Format, RemoveValidatorRequest,
+  Schema,
+  SetParentRequest, Validator
+} from "../../../proto/lib/messages/riiid/kvf";
+import {Type} from "../../../proto/lib/messages/riiid/kvf/Format";
+
+export async function createFormat(pool: Pool, {formatName, formatDescription}: CreateFormatRequest): Promise<Format> {
+  const client = await pool.connect()
+  const queryConfig = {
+    text: `INSERT INTO formats(name, description) VALUES($1, $2) RETURNING *`,
+    values: [formatName, formatDescription]
+  }
+  const res = await client.query(queryConfig)
+  const format = res.rows[0]
+  await client.release()
+  return format
+}
+
+export async function deleteFormat(
+  pool: Pool,
+  { formatId}: DeleteFormatRequest
+) {
+  const client = await pool.connect()
+  const sql = `DELETE FROM formats WHERE id = ${formatId}`
+  await client.query(sql)
+  await client.release()
+}
+
+export async function addParentFormatId(
+  pool: Pool,
+  {formatId, parentFormatId}: SetParentRequest
+) {
+  const client = await pool.connect()
+  const queryConfig = {
+    text: `UPDATE formats SET "parentFormatId" = $1 WHERE id = $2`,
+    values: [parentFormatId, formatId]
+  }
+  await client.query(queryConfig)
+  await client.release()
+}
+
+export async function deleteParentFormatId(
+  pool: Pool,
+  {formatId}: DeleteFormatRequest
+) {
+  const client = await pool.connect()
+  const sql = `UPDATE formats SET "parentFormatId" = 'undefined' WHERE id = ${formatId}`
+  await client.query(sql)
+  client.release()
+}
+
+export async function createValidator(
+  pool: Pool,
+  {formatId, validatorName, validatorDescription, validatorSrc, validatorIntegrity}: AppendValidatorRequest
+) {
+  const client = await pool.connect()
+  const validatorQueryConfig = {
+    text: `INSERT INTO validators(name, description, src, integrity) VALUES($1, $2, $3, $4) RETURNING *`,
+    values: [validatorName, validatorDescription, validatorSrc, validatorIntegrity]
+  }
+  const validator: Validator = (await client.query(validatorQueryConfig)).rows[0]
+  client.release()
+  const format = await getFormat(pool, formatId)
+  const validatorIds = (await getValidatorIds(format.validatorIds, validator.id)).map(res => res.toString())
+  await updateFormatValidatorIds(pool, formatId, validatorIds)
+  return {
+    validatorId: validator.id
+  }
+}
+
+export async function deleteValidator(
+  pool: Pool,
+  {formatId, validatorId}: RemoveValidatorRequest
+) {
+  const client = await pool.connect()
+  const deleteValidatorSql = `DELETE FROM validators WHERE id = ${validatorId}`
+  await client.query(deleteValidatorSql)
+  const format = await getFormat(pool, formatId)
+  const validatorIds = format.validatorIds.filter((formatValidatorId: string) => formatValidatorId != validatorId)
+  await updateFormatValidatorIds(pool, formatId, validatorIds)
+  client.release()
+}
+
+export async function createConverter(
+  pool: Pool,
+  {fromFormatId, toFormatId, converterName, converterDescription, converterSrc, converterIntegrity}: CreateConverterRequest
+) {
+  const client = await pool.connect()
+  const converterSqlConfig = {
+    text: `INSERT INTO converters(name, description, src, integrity) VALUES($1, $2, $3, $4) RETURNING *`,
+    values: [converterName, converterDescription, converterSrc, converterIntegrity]
+  }
+  const converter = (await client.query(converterSqlConfig)).rows[0]
+  await createEdge(pool, fromFormatId, toFormatId, converter.id)
+  return {
+    converterId: converter.id
+  }
+}
+
+export async function deleteConverter(
+  pool: Pool,
+  {converterId}: DeleteConverterRequest
+) {
+  const client = await pool.connect()
+  const queryConfig = {
+    text: `DELETE FROM converters WHERE id = $1`,
+    values: [converterId]
+  }
+  await client.query(queryConfig)
+  await deleteEdge(pool, converterId)
+}
+
+export async function deleteEdge(pool: Pool, converterId: string) {
+  const client = await pool.connect()
+  const queryConfig = {
+    text: `DELETE FROM edges WHERE "converterId" = $1`,
+    values: [converterId]
+  }
+  await client.query(queryConfig)
+}
+
+export async function createEdge(pool: Pool, fromFormatId: string, toFormatId: string, converterId: string) {
+  const client = await pool.connect()
+  const sqlConfig = {
+    text: `INSERT INTO edges("fromFormatId", "toFormatId", "converterId") VALUES($1, $2, $3)`,
+    values: [fromFormatId, toFormatId, converterId]
+  }
+  await client.query(sqlConfig)
+}
+
+export async function getFormat(
+  pool: Pool,
+  formatId: string
+) {
+  const client = await pool.connect()
+  const sql = `SELECT * FROM formats WHERE id = ${formatId}`
+  const format = (await client.query(sql)).rows[0]
+  client.release()
+  return format
+}
+
+export async function updateFormatValidatorIds(
+  pool: Pool,
+  formatId: string,
+  validatorIds: string[]
+) {
+  const client = await pool.connect()
+  const queryConfig = {
+    text: `UPDATE formats SET "validatorIds" = $1 WHERE id = $2`,
+    values: [validatorIds, formatId]
+  }
+  await client.query(queryConfig)
+  client.release()
+}
+export async function getValidatorIds(validatorIds: string[], validatorId: string) {
+  if (validatorIds) {
+    return [...validatorIds, validatorId]
+  } else {
+    return [validatorId]
+  }
+}
+
+export async function getSchema(pool: Pool): Promise<Schema> {
+  const client = await pool.connect()
+  const formatSql = "SELECT * FROM formats"
+  const formats: Format[] = (await client.query(formatSql)).rows
+  const edgeSql = "SELECT * FROM edges"
+  const edges = await client.query(edgeSql)
+  const validatorSql = "SELECT * FROM validators"
+  const validators = await client.query(validatorSql)
+  const converterSql = "SELECT * FROM converters"
+  const converters = await client.query(converterSql)
+  const schema: Schema = {
+    formats: formats,
+    edges: edges.rows,
+    validators: validators.rows,
+    converters: converters.rows
+  }
+  await client.release()
+  return schema
+}
